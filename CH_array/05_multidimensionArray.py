5798f968416dd9614997b3a5ce82adeda5d2cce6
Mohammad Sahil <mohdsahil00736@gmail.com>
2026-07-28 23:28:57 +0530

    multi_dimension_array

---
+++ CH_array/05_multidimensionArray.py
@@ -0,0 +1,14 @@
+from numpy import * 
+
+zero = array(10)
+print(zero)
+print('\n')
+
+ones = array([1, 2, 3, 4])
+print(ones, "\n")
+
+two = array([[1,3], [5, 7]])
+print(two, '\n')
+
+three = array([[[1, 2],[3, 4]], [[5, 6],[7, 8]]])
+print(three, '\n')
