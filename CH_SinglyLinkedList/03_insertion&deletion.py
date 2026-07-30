412884eb43b9d638b4c7273125007da7fbe826f6
Mohammad Sahil <mohdsahil00736@gmail.com>
2026-07-30 19:55:19 +0530

    insertion & deletion

--- CH_SinglyLinkedList/03_insertion&deletion.py
+++ CH_SinglyLinkedList/03_insertion&deletion.py
@@ -24,6 +24,20 @@ class SinglyLinkedList:
                 t1.next = temp
             t1 = t1.next
 
+    def deleteLL(self, value ):
+        t1 = self.head
+        prev = t1
+        if (t1.data == value):
+            self.head = t1.next
+        while(t1.next != None):
+            if(t1.data == value):
+                prev.next = t1.next
+                break
+            else :
+                prev = t1
+                t1 = t1.next
+        if (t1.data == value):
+            prev.next = None
      
     def PrintLinkedList(self):
         t1 = self.head
@@ -37,6 +51,9 @@ obj = SinglyLinkedList()
 obj.InsertAtBeg(5)
 obj.InsertAtBeg(8)
 obj.InsertAtBeg(1)
-obj.InsertAtMid(8, 10)
+obj.InsertAtMid(10, 8)
+obj.InsertAtMid(20, 10)
+obj.deleteLL(20)
 obj.PrintLinkedList()
+
 
