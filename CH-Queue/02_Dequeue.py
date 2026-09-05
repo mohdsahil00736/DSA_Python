8df085f629515f74b1e2a9da5a17351f64fe3c36
Mohammad Sahil <mohdsahil00736@gmail.com>
2026-09-06 00:31:10 +0530

    Dequeue

--- CH-Queue/02_Dequeue.py
+++ CH-Queue/02_Dequeue.py
@@ -1,4 +1,4 @@
-class Queuue:
+class Dequeuue:
 
     def __init__(self):
         self.items = []
@@ -11,13 +11,30 @@ class Queuue:
 
     def DeleteAtBeg(self):
         if self.IsEmpty():
-            raise Exception("Queue is Empty")
+            raise Exception("Dequeue is Empty")
         else:
             return self.items.pop(0)
 
-qu = Queuue()
+    def InsertAtBeg(self, value):       
+        return self.items.insert(0, value)
+
+    def DeleteATEnd(self):
+        if self.IsEmpty():
+            raise Exception("Dequeue is Empty")
+        else:
+            return self.items.pop()
+
+
+qu = Dequeuue()
 qu.InsertAtEnd(10)
+qu.InsertAtBeg(50)
 qu.InsertAtEnd(20)
 qu.InsertAtEnd(30)
+qu.InsertAtBeg(90)
+print(qu.items)
+print(qu.DeleteATEnd())
+print(qu.DeleteATEnd())
 print(qu.DeleteAtBeg())
 print(qu.DeleteAtBeg())
+print(qu.DeleteATEnd())
+qu.DeleteATEnd()
